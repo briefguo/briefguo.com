c29561a2bb44e61cd6a8f4c592efdbaca49eed6c
briefguo <gyj9094@gmail.com>
2020-01-23 17:56:04 +0800

    fix: 去掉最小宽度

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -12,11 +12,6 @@ const Body: React.FC<BodyProps> = props => {
       color:${props.color};
       background-color:${props.backgroundColor}
     }
-    @media (max-width: 768px) {
-      body {
-        min-width: 375px;
-      }
-    }
   `
   return (
     <>

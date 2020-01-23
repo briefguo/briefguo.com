766ff7e74d082da2e33033e35a7581e431815321
briefguo <gyj9094@gmail.com>
2020-01-23 17:40:48 +0800

    chore: lint文件

--- src/typings/types.d.ts
+++ src/typings/types.d.ts
@@ -4,35 +4,35 @@
 
 declare namespace NodeJS {
   interface ProcessEnv {
-    readonly NODE_ENV: 'development' | 'production' | 'test';
-    readonly PUBLIC_URL: string;
+    readonly NODE_ENV: 'development' | 'production' | 'test'
+    readonly PUBLIC_URL: string
   }
 }
 
 declare module '*.jpg' {
-  const src: string;
-  export default src;
+  const src: string
+  export default src
 }
 
 declare module '*.jpeg' {
-  const src: string;
-  export default src;
+  const src: string
+  export default src
 }
 
 declare module '*.png' {
-  const src: string;
-  export default src;
+  const src: string
+  export default src
 }
 
 declare module '*.svg' {
-  import * as React from 'react';
+  import * as React from 'react'
 
   export const ReactComponent: React.FunctionComponent<React.SVGProps<
     SVGSVGElement
-  >>;
+  >>
 
-  const src: string;
-  export default src;
+  const src: string
+  export default src
 }
 
-declare module '*.less';
+declare module '*.less'
